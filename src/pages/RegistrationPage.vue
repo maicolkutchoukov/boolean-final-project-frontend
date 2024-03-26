aa58f0f035fd88a072a7ffc87a63bdc8634aa9ad
StefanoDellamore <146744495+StefanoDellamore@users.noreply.github.com>
2024-03-26 18:11:25 +0100

    Create registration page

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -10,26 +10,75 @@ export default {
 </script>
 
 <template>
-    CIAO SONO LA PAGINA DI REGISTRAZIONE
-    <div class="scroll-watcher"></div>
+    <div class="container mt-3">
+        
+        <h2>REGISTRAZIONE</h2>
+        <form>
+            <div class="d-flex justify-content-around">
+                <div>
+                <!--Input nome-->
+                <div class="form-group">
+                    <label for="name">Nome*</label>
+                    <input type="name" class="form-control mb-2" id="name" placeholder="Inserisci il tuo nome">               
+                </div>
+
+                <!--Input città-->
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Città*</label>
+                    <input type="password" class="form-control mb-2" id="exampleInputPassword1" placeholder="Inserisci la tua città">
+                </div>
+            </div>
+
+            <div>
+                <!--Input Email-->
+                <div class="form-group">
+                    <label for="email">Mail*</label>
+                    <input type="email" class="form-control mb-2" id="email" placeholder="Inserisci la tua email">                
+                </div>
+
+                <!--Input password-->
+                <div class="form-group">
+                    <label for="password">Password*</label>
+                    <input type="password" class="form-control mb-2" id="password" placeholder="Inserisci password">
+                </div>
+
+                <!--Input conferma password-->
+                <div class="form-group">
+                    <label for="password">Conferma Password*</label>
+                    <input type="password" class="form-control mb-2" id="password" placeholder="Conferma password">
+                </div>
+            </div>
+        
+        </div>
+
+        <div class="d-flex justify-content-around mt-3">
+
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Dichiaro di aver letto le condizioni legali
+                    </label>
+                </div>
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Dichiaro di aver letto la privacy policy
+                    </label>
+            </div>
+            
+        </div>
+
+        <div class="text-center">
+            <button type="submit" class="btn btn-primary">Registrati</button>
+        </div>
+            
+        </form>
+    </div>
 
 </template>
 
 <style lang="scss" scoped>
 
-.scroll-watcher{
-    height: 5px;
-    position: fixed;
-    top: 0;
-    z-index: 9999;
-    background-color: black;
-    width: 100%;
-    scale: 0 1;
-    transform-origin: left;
-    animation: scroll-watcher linear;
-    animation-timeline: scroll(y);
-}
-@keyframes scroll-watcher{
-    to { scale: 1 1;}
-}
 </style>
+
+<!-- nome, email,password, città -->
