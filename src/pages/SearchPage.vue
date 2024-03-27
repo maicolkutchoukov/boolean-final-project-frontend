c6b5e3430c90e5099a9213b980b3921d31950498
StefanoDellamore <146744495+StefanoDellamore@users.noreply.github.com>
2024-03-27 19:07:55 +0100

    Add partial structure SearchPage

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -4,19 +4,127 @@
 export default {
     data() {
         return {
+            CheckBoxData: [
+                {
+                    title: 'Cantanti',
+                },
+                {
+                    title: 'Bassista',
+                },
+                {
+                    title: 'Violista',
+                },
+            ]
         }
      },
 }
 </script>
 
 <template>
-    CIAO SONO LA PAGINA DI RICERCA
+    <div class="container-fluid d-flex">
+        <div class="aside-container">
+            <!-- Aside-container -->
+            <form>
+                <div class="form-group w-75">
+                    <label for="exampleInputEmail1">Search</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Inserisci il nome dell'artista">
+                </div>
+                <div class="form-group w-50">
+                    <label for="exampleInputEmail1">Città</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Inserisci la città">
+                </div>
+                
+                <h5 class="mt-3">Tipi di musicisti:</h5>
+                <div v-for="(CheckBox, i) in CheckBoxData" :key="i" class="form-check form-check-inline d-flex mb-2">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                    <label class="form-check-label" for="inlineCheckbox1">{{ CheckBox.title }}</label>
+                </div>
+                
+
+                <button type="submit" class="btn btn-primary mt-2 mb-2">Submit</button>
+            </form>
+        </div>
+        <div class="main-container">
+            <!-- main-container -->
+            <div class="row d-flex">                    
+                    <div class="col-3">
+                        <div class="card-result d-flex flex-column">
+                            <div class="card-result-top">
+                                <div class="background-img">background-img</div>
+                                <div class="user-icon">user icon</div>
+                            </div>
+                            <div class="card-result-bottom"></div>
+                        </div>
+                    </div>
+                    <!--
+                    <div class="col-3 ">
+                        <div class="card-result"></div>
+                    </div>
+                    <div class="col-3 ">
+                        <div class="card-result"></div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card-result"></div>
+                    </div>
+                    -->                                   
+            </div>
+
+        </div>
+    </div>
+
     <div class="scroll-watcher"></div>
 
 </template>
 
 <style lang="scss" scoped>
 
+/* Aside Container */
+.aside-container {
+    border: 1px dashed black;
+    width: 400px;
+    min-height: 500px; 
+}
+
+/* Main Container */
+
+.main-container {
+    border: 1px dashed red;
+    width: calc(100% - 400px);
+    min-height: 500px;
+}
+
+/* Card-result */
+
+.card-result {
+    border: 1px dashed black;
+    border-radius: 20px;
+    min-height: 300px;
+}
+
+.card-result-top {
+    border: 1px dashed violet;
+    height: 150px;
+}
+
+.card-result-bottom {
+    border: 1px dashed blue;
+    height: 100px;
+}
+
+.container-img {
+    border: 1px solid red;
+    height: 100px;
+    width: 100px;
+}
+
+.user-icon {
+    border: 1px solid green;
+    height: 70px;
+    width: 70px;
+    border-radius: 50px;
+}
+
+/*
 .scroll-watcher{
     height: 5px;
     position: fixed;
@@ -32,4 +140,5 @@ export default {
 @keyframes scroll-watcher{
     to { scale: 1 1;}
 }
+*/
 </style>
