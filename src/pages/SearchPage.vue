b3d9e92e2d7a2caa16a3193a10bafe6f9d5fb225
maicolkutchoukov <maicol.j.95@gmail.com>
2024-04-03 11:19:38 +0200

    FIXED ERROR SEARCHPAGE

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -2,45 +2,63 @@
 import axios from 'axios';
 
 export default {
-  data() {
-    return {
-        allMusicians: [],
-        allRoles: [],
-        selectedRoles: [], // Array per memorizzare i ruoli selezionati
-        searchQuery: ''
-    };
-  },
-  methods: {
-    toggleRole(role) {
-        // Aggiungi o rimuovi il ruolo selezionato dall'array
-        if (this.selectedRoles.includes(role)) {
-            this.selectedRoles = this.selectedRoles.filter(selectedRole => selectedRole !== role);
-        } else {
-            this.selectedRoles.push(role);
-        }
-        },
-    isSelected(role) {
-        // Verifica se il ruolo è selezionato
-        return this.selectedRoles.includes(role);
+    data() {
+        return {
+            allMusicians: [],
+            allRoles: [],
+            selectedRoles: [],
+            searchQuery: ''
+        };
     },
-    search() {
-            console.log(this.selectedRoles)
-        // Esegui la ricerca solo se ci sono ruoli selezionati
-        if (this.selectedRoles.length > 0 || this.searchQuery.trim() !== '') {
-            axios.get('http://127.0.0.1:8000/api/users', { params: { roles: this.selectedRoles, query: this.searchQuery }})
-            .then(response => {
-                console.log(response.data);
-                // Gestisci la risposta della richiesta API qui
-            })
-            .catch(error => {
-                console.error('Errore durante la chiamata API:', error);
-            });
-        } else {
-            console.log('Seleziona almeno un ruolo o inserisci un termine di ricerca.');
+    computed: {
+        filteredMusicians() {
+            let filtered = this.allMusicians;
+
+            // Filtrare per ruoli selezionati, se presenti
+            if (this.selectedRoles.length > 0) {
+                filtered = filtered.filter(musician => musician.roles.some(role => this.selectedRoles.includes(role.title)));
+            }
+
+            // Filtrare per query di ricerca, se presente
+            if (this.searchQuery.trim() !== '') {
+                filtered = filtered.filter(musician => musician.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+            }
+
+            return filtered;
         }
-    }
-  },
-  created() {
+    },
+    methods: {
+        toggleRole(role) {
+            if (this.selectedRoles.includes(role)) {
+                this.selectedRoles = this.selectedRoles.filter(selectedRole => selectedRole !== role);
+            } else {
+                this.selectedRoles.push(role);
+            }
+        },
+        isSelected(role) {
+            return this.selectedRoles.includes(role);
+        },
+        search() {
+            // Questo metodo viene chiamato quando viene inviato il modulo di ricerca
+            // Qui puoi implementare la logica per effettuare la richiesta API di ricerca
+            // Utilizzando this.searchQuery e this.selectedRoles
+            axios.get('http://127.0.0.1:8000/api/users', {
+                params: {
+                    name: this.searchQuery,
+                    roles: this.selectedRoles.join(',')
+                }
+            })
+                .then(response => {
+                    // Aggiorna allMusicians con i dati ricevuti dalla risposta API
+                    this.allMusicians = response.data.results.data;
+                })
+                .catch(error => {
+                    console.error('Errore durante la chiamata API:', error);
+                });
+        }
+    },
+    mounted() {
+        // Ottieni la lista di tutti i musicisti e i ruoli al caricamento del componente
         axios.get('http://127.0.0.1:8000/api/users')
             .then(response => {
                 this.allMusicians = response.data.results.data;
@@ -63,43 +81,43 @@ export default {
 <template>
     <div class="container-fluid">
         <h2 class="p-5 fw-bold">Trova artisti o band</h2>
-            <form>
-                <div class="mb-5">
-                    <input type="text" v-model="searchQuery" class="form-control w-50 input-searchbar" name="searchArtist" placeholder="Cerca artisti o band...">
+        <form @submit.prevent="search">
+            <div class="mb-5">
+                <input type="text" v-model="searchQuery" class="form-control w-50 input-searchbar" placeholder="Cerca artisti o band...">
+            </div>
+            <div class="container d-flex flex-wrap text-center mb-5">
+                <div v-for="(role, index) in allRoles" :key="index" class="mycol-2">
+                    <button class="button-roles"
+                            type="button"
+                            :class="{ 'active': isSelected(role.title) }"
+                            @click="toggleRole(role.title)">
+                        {{ role.title }}
+                    </button>
                 </div>
-                <div class="container d-flex flex-wrap text-center mb-5">
-                    <div v-for="(role, index) in allRoles" :key="index"  class="mycol-2">
-                        <button class="button-roles"
-                                type="button"
-                                :class="{ 'active': isSelected(role.title) }" 
-                                @click="toggleRole(role.title)">
-                            {{ role.title }}
-                        </button>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary px-5">Cerca</button>
+            </div>
+        </form>
+    </div>
+    <div class="container-fluid">
+        <h2 class="p-5 fw-bold">Artisti in evidenza</h2>
+        <div class="row g-3">
+            <div class="col-3" v-for="(singleMusician, index) in filteredMusicians" :key="singleMusician.id">
+                <div class="card">
+                    <div>
+                        <img :src="'http://127.0.0.1:8000/storage/' + singleMusician.user_details.picture" class="card-img-top" alt="Immagine artista">
                     </div>
-                </div>
-                <div class="text-center">
-                    <button type="button" class="btn btn-primary px-5" @click="search">Cerca</button>
-                </div>
-            </form>
-        </div>
-        <div class="container-fluid">
-            <h2 class="p-5 fw-bold">Artisti in evidenza</h2>
-            <div class="row g-3">
-                <div class="col-3" v-for="(singleMusician, index) in allMusicians" :key="singleMusician.id">
-                    <div class="card">
-                        <div>
-                            <img :src="'http://127.0.0.1:8000/storage/' + singleMusician.user_details.picture" class="card-img-top" alt="Immagine artista">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ singleMusician.name }}</h5>
-                            <p class="card-text">{{ singleMusician.city }}</p>
-                            <!-- <a :href="singleMusician.link" class="btn btn-primary">Dettagli</a> -->
-                            <router-link :to="{ name: 'profile', params: { id: singleMusician.id} }" class="btn btn-primary btn-sm">Vedi Profilo</router-link>
-                        </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ singleMusician.name }}</h5>
+                        <p class="card-text">{{ singleMusician.city }}</p>
+                        <!-- <a :href="singleMusician.link" class="btn btn-primary">Dettagli</a> -->
+                        <router-link :to="{ name: 'profile', params: { id: singleMusician.id} }" class="btn btn-primary btn-sm">Vedi Profilo</router-link>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
