b1a1f92088b95622f6b240d388dca7e31ee631a7
maicolkutchoukov <maicol.j.95@gmail.com>
2024-04-04 18:54:20 +0200

    Add Form Vote/reviews on ProfilePage

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,71 +1,105 @@
-<!-- PAGINA PRINCIPALE DOVE ANDRANNO INSERITI TUTTI I COMPONENTI  -->
-
 <script>
-
 import axios from 'axios';
 
-// Import Swiper styles
-import 'swiper/css';
-
-import 'swiper/css/pagination';
-
-// import required modules
-import { Pagination } from 'swiper/modules';
-
 export default {
-    data() {
-        return {
-            apiUrl: 'http://127.0.0.1:8000/api/users/',
-            singleMusician: [],
-            demoPath: '',
-            contactForm: {
-                firstname: '',
-                lastname: '',
-                email:'',
-                message: '',
-                user_id: '',
-            }
-        };
-    },
-
-    methods: {
-        sendMessage() {
-            axios.post('http://127.0.0.1:8000/api/messages', this.contactForm)
-                .then(response => {
-                    console.log('Messaggio inviato con successo:', response.data);
-                    this.resetForm();
-                    alert('Messaggio inviato con successo')
-                })
-                .catch(error => {
-                    console.error('Errore durante l\'invio del messaggio:', error);
-                    alert('Errore invio messaggio')
-                });
+  data() {
+    return {
+        apiUrl: 'http://127.0.0.1:8000/api/',
+        singleMusician: [],
+        demoPath: '',
+        contactForm: {
+            firstname: '',
+            lastname: '',
+            email: '',
+            message: '',
+            user_id: '',
+            reviewText: '',
+            rating: null
+        }
+    };
+},
+methods: {
+    sendMessage() {
+        axios.post(`${this.apiUrl}messages`, this.contactForm)
+            .then(response => {
+                console.log('Messaggio inviato con successo:', response.data);
+                this.resetForm();
+                alert('Messaggio inviato con successo');
+            })
+            .catch(error => {
+                console.error('Errore durante l\'invio del messaggio:', error);
+                alert('Errore invio messaggio');
+            });
         },
         resetForm() {
-            // Resettare i valori del form dopo l'invio del messaggio
             this.contactForm.firstname = '';
             this.contactForm.lastname = '';
             this.contactForm.email = '';
             this.contactForm.message = '';
+        },
+    submitReviewAndVote() {
+        if (!this.contactForm.reviewText && !this.contactForm.rating) {
+            alert('Per favore, inserisci almeno una recensione o un voto');
+            return;
         }
-    },
-    created() {
-        axios.get(`${this.apiUrl}${this.$route.params.name}`) // URL DELL'API
-            .then((response) => {
-                console.log(response.data.result);
-                this.singleMusician = response.data.result;
-                console.log(this.singleMusician)
-                this.demoPath = 'http://127.0.0.1:8000/storage' + this.singleMusician.user_details.demo
-                console.log(this.demoPath)
-                this.contactForm.user_id = this.singleMusician.id
+
+        // Invia la richiesta API per salvare la recensione
+        if (this.contactForm.reviewText) {
+            axios.post(`${this.apiUrl}reviews`, {
+            reviewText: this.contactForm.reviewText,
+            userId: this.contactForm.user_id
+            })
+            .then(response => {
+                console.log('Recensione inviata con successo:', response.data);
+                this.resetReviewForm();
+                alert('Recensione inviata con successo');
             })
             .catch(error => {
+                console.error('Errore durante l\'invio della recensione:', error);
+                alert('Errore durante l\'invio della recensione');
+            });
+        }
+
+        // Invia la richiesta API per salvare il voto
+        if (this.contactForm.rating) {
+            axios.post(`${this.apiUrl}votes`, {
+            label: 'Rating',
+            vote: this.contactForm.rating,
+            userId: this.contactForm.user_id
+            })
+            .then(response => {
+                console.log('Voto inviato con successo:', response.data);
+                this.resetRating();
+                alert('Voto inviato con successo');
+            })
+            .catch(error => {
+                console.error('Errore durante l\'invio del voto:', error);
+                alert('Errore durante l\'invio del voto');
+            });
+        }
+    },
+    resetReviewForm() {
+        this.contactForm.reviewText = '';
+    },
+    resetRating() {
+        this.contactForm.rating = null;
+    }
+},
+    created() {
+        axios.get(`${this.apiUrl}users/${this.$route.params.name}`)
+        .then(response => {
+            console.log(response.data.result);
+            this.singleMusician = response.data.result;
+            console.log(this.singleMusician);
+            this.demoPath = `http://127.0.0.1:8000/storage${this.singleMusician.user_details.demo}`;
+            console.log(this.demoPath);
+            this.contactForm.user_id = this.singleMusician.id;
+        })
+        .catch(error => {
             console.error('Errore durante la chiamata API:', error);
         });
-    },
-}
-
-
+    }
+};
 </script>
 
 <template>
@@ -78,34 +112,32 @@ export default {
         <div class="row justify-content-between px-5">
             <div class="col-6">
                 <h2 class="fw-bold pt-3 mb-4">Bio</h2>
-                <p v-if="singleMusician.user_details" >{{ singleMusician.user_details.bio }}</p>
+                <p v-if="singleMusician.user_details">{{ singleMusician.user_details.bio }}</p>
             </div>
             <div class="col-4 text-end">
                 <h2 class="fw-bold pt-3 mb-4 ">Competenze</h2>
                 <div class="">
                     <span v-for="(role, i) in singleMusician.roles" :key="i">
-                        
-                        <img :src='"http://127.0.0.1:8000/storage/" + role.icon' alt="roleInstrument" class="instrument-pic px-4">
-                        <!-- <span v-if="i !== singleMusician.roles.length - 1">, </span> -->
+                    <img :src="'http://127.0.0.1:8000/storage/' + role.icon" alt="roleInstrument" class="instrument-pic px-4">
                     </span>
                 </div>
             </div>
             <div class="col-12">
                 <h2 class="fw-bold pt-3 mb-4">Demo</h2>
                 <audio v-if="singleMusician.user_details" controls class="w-100 px-5">
-                    <source :src="this.demoPath" type="audio/mpeg">
+                    <source :src="demoPath" type="audio/mpeg">
                 </audio>
             </div>
             <div class="col-12">
                 <h2 class="fw-bold pt-3 mb-4">Info di contatto</h2>
-                <div v-if="singleMusician.user_details" >
-                    <i class="fa-solid fa-phone me-3"></i>
-                    Mail: {{ singleMusician.user_details.cellphone }}
-                </div>
-                <div>
-                    <i class="fa-solid fa-envelope me-3"></i>
-                    Cell: {{ singleMusician.email }}
-                </div>
+            <div v-if="singleMusician.user_details">
+                <i class="fa-solid fa-phone me-3"></i>
+                Mail: {{ singleMusician.user_details.cellphone }}
+            </div>
+            <div>
+                <i class="fa-solid fa-envelope me-3"></i>
+                Cell: {{ singleMusician.email }}
+            </div>
             </div>
         </div>
         <div class="container">
@@ -115,16 +147,7 @@ export default {
                         <div class="card-body">
                             <h4 class="card-title py-4">Federico Benevento</h4>
                             <p class="card-text pe-4">
-                                Questo concerto ha creato delle
-                                atmosfere magiche che mi hanno rapito
-                                fin dal primo momento.
-                                Il perfomer ha saputo coinvolgere il
-                                pubblico con la sua presenza
-                                carismatica e le sue performance
-                                straordinarie. La musica ha trasportato
-                                tutti in un viaggio
-                                emotivo che non dimenticherò
-                                facilmente.
+                            Questo concerto ha creato delle atmosfere magiche che mi hanno rapito fin dal primo momento. Il perfomer ha saputo coinvolgere il pubblico con la sua presenza carismatica e le sue performance straordinarie. La musica ha trasportato tutti in un viaggio emotivo che non dimenticherò facilmente.
                             </p>
                             <div>STELLE</div>
                         </div>
@@ -135,19 +158,9 @@ export default {
                         <div class="card-body">
                             <h4 class="card-title py-4">Federico Benevento</h4>
                             <p class="card-text pe-4">
-                                Questo concerto ha creato delle
-                                atmosfere magiche che mi hanno rapito
-                                fin dal primo momento.
-                                Il perfomer ha saputo coinvolgere il
-                                pubblico con la sua presenza
-                                carismatica e le sue performance
-                                straordinarie. La musica ha trasportato
-                                tutti in un viaggio
-                                emotivo che non dimenticherò
-                                facilmente.
+                            Questo concerto ha creato delle atmosfere magiche che mi hanno rapito fin dal primo momento. Il perfomer ha saputo coinvolgere il pubblico con la sua presenza carismatica e le sue performance straordinarie. La musica ha trasportato tutti in un viaggio emotivo che non dimenticherò facilmente.
                             </p>
                             <div>STELLE</div>
-                            
                         </div>
                     </div>
                 </div>
@@ -156,19 +169,9 @@ export default {
                         <div class="card-body">
                             <h4 class="card-title py-4">Federico Benevento</h4>
                             <p class="card-text pe-4">
-                                Questo concerto ha creato delle
-                                atmosfere magiche che mi hanno rapito
-                                fin dal primo momento.
-                                Il perfomer ha saputo coinvolgere il
-                                pubblico con la sua presenza
-                                carismatica e le sue performance
-                                straordinarie. La musica ha trasportato
-                                tutti in un viaggio
-                                emotivo che non dimenticherò
-                                facilmente.
+                            Questo concerto ha creato delle atmosfere magiche che mi hanno rapito fin dal primo momento. Il perfomer ha saputo coinvolgere il pubblico con la sua presenza carismatica e le sue performance straordinarie. La musica ha trasportato tutti in un viaggio emotivo che non dimenticherò facilmente.
                             </p>
                             <div>STELLE</div>
-                            
                         </div>
                     </div>
                 </div>
@@ -178,33 +181,46 @@ export default {
             </div>
         </div>
     </div>
+    <div>
+      <!-- Form per inserire una recensione e un voto -->
+        <form @submit.prevent="submitReviewAndVote">
+            <input type="text" v-model="contactForm.reviewText" placeholder="Inserisci la tua recensione">
+            <input type="number" v-model="contactForm.rating" placeholder="Voto da 1 a 5">
+            <button type="submit">Invia recensione e voto</button>
+        </form>
+    </div>
     <section class="contact-section">
         <div class="container">
             <h2 class="text-white text-center py-5">Contatta l'utente!</h2>
-            <form @submit.prevent="sendMessage()">
-                <div class="mb-3">
-                    <label for="firstname" class="form-label text-white">Nome*</label>
-                    <input type="text" v-model="contactForm.firstname" class="form-control" id="firstname" placeholder="Inserisci il tuo nome..." required maxlength="74">
-                </div>
-                <div class="mb-3">
-                    <label for="lastname" class="form-label text-white">Cognome*</label>
-                    <input type="text" v-model="contactForm.lastname" class="form-control" id="lastname" placeholder="Inserisci il tuo cognome..." required maxlength="74">
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label text-white">Email*</label>
-                    <input type="email" v-model="contactForm.email" class="form-control" id="email" placeholder="Inserisci la tua mail..." required maxlength="255">
-                </div>
-                <div class="mb-3">
-                    <label for="message" class="form-label text-white">Messaggio*</label>
-                    <textarea v-model="contactForm.message" class="form-control" id="message" rows="5" required maxlength="2048" placeholder="Lascia un messaggio che verrà visualizzato dall'artista..."></textarea>
-                </div>
-                <div class="text-center py-5">
-                    <button type="submit" class="btn px-5 my-button btn-lg">Invia</button>
-                </div>
+            <form @submit.prevent="sendMessage">
+            <div class="mb-3">
+                <label for="firstname" class="form-label text-white">Nome*</label>
+                <input type="text" v-model="contactForm.firstname" class="form-control" id="firstname"
+                placeholder="Inserisci il tuo nome..." required maxlength="74">
+            </div>
+            <div class="mb-3">
+                <label for="lastname" class="form-label text-white">Cognome*</label>
+                <input type="text" v-model="contactForm.lastname" class="form-control" id="lastname"
+                placeholder="Inserisci il tuo cognome..." required maxlength="74">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label text-white">Email*</label>
+                <input type="email" v-model="contactForm.email" class="form-control" id="email"
+                placeholder="Inserisci la tua mail..." required maxlength="255">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label text-white">Messaggio*</label>
+                <textarea v-model="contactForm.message" class="form-control" id="message" rows="5" required
+                maxlength="2048"
+                placeholder="Lascia un messaggio che verrà visualizzato dall'artista..."></textarea>
+            </div>
+            <div class="text-center py-5">
+                <button type="submit" class="btn px-5 my-button btn-lg">Invia</button>
+            </div>
             </form>
         </div>
     </section>
-</template>
+  </template>
 
 <style lang="scss" scoped>
 
