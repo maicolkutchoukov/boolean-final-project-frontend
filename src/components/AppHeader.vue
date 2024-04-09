38d4d996c3352a39f56e25aa4c7cbb598e365185
maicolkutchoukov <maicol.j.95@gmail.com>
2024-04-09 09:40:18 +0200

    Fix Header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,176 +1,105 @@
 <script>
 export default {
-    data() {
-        return {
-
-        };
-    },
-    methods: {
-
+ /*  methods: {
+    startAnimation(event) {
+      const link = event.target;
+      link.style.transform = 'translateX(100%)';
+      setTimeout(() => {
+        link.style.transform = 'translateX(0)';
+      }, 500); 
     }
-}
+  } */
+};
 </script>
 
 <template>
     <header>
-       <div id="container-header" class="container-fluid">
-            <div class="row h-100">
-
-                <!--Logo-->
-                <div class="col-4 d-flex align-items-center">
-                    <div class="container-logo">
-                        <router-link :to="{ name: 'homme' }">
-                            <img src="../../public/Img/LogoBMusic.png" class="h-100"t="LogoBMusic">
-                        </router-link>
-                        
-                    </div>                    
-                </div>
-
-                <!--Link-->
-                <div class="col-8 d-flex align-items-center justify-content-end">
-                    <div class="container-link">
-                        <nav>
-                            <ul class="d-flex">
-                                <li>
-                                    <router-link :to="{ name: 'instrumental' }" class="header-link">Strumenti</router-link>
-                                </li>
-                                <li>
-                                    <router-link :to="{ name: 'sponsor' }" class="header-link">Sponsor</router-link>
-                                </li>
-                                <li>
-                                    <a href="http://127.0.0.1:8000/login" class="header-link">Accedi</a>
-                                </li>
-                                
-                                <li>
-                                    <a href="http://127.0.0.1:8000/register" class="header-link">Registrati</a>
-                                </li>
-                                <li>
-                                    <router-link :to="{ name: 'search' }" class="header-link-special">Cerca</router-link>
-                                </li> 
-                            </ul>
-                        </nav>
-
-                        <!-- Default dropstart button -->
-                        <div class="btn-group dropdown">
-                            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                Menu
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li class="dropdown-menu-link">
-                                    <router-link :to="{ name: 'instrumental' }" class="link-style">Strumenti</router-link>
-                                </li>
-                                <li class="dropdown-menu-link">
-                                    <router-link :to="{ name: 'sponsor' }" class="link-style">Sponsor</router-link>
-                                </li>
-                                <li class="dropdown-menu-link">
-                                    <a href="http://127.0.0.1:8000/login" class="link-style">Accedi</a>
-                                </li>
-                                
-                                <li class="dropdown-menu-link">
-                                    <a href="http://127.0.0.1:8000/register" class="link-style">Registrati</a>
-                                </li>
-                                <li class="dropdown-menu-link">
-                                    <router-link :to="{ name: 'search' }" class="link-style">Cerca</router-link>
-                                </li> 
-                            </ul>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-       </div>
+      <nav class="navbar navbar-expand-lg navbar-dark my-navbar">
+        <div class="container">
+          <router-link :to="{ name: 'homme' }" class="navbar-brand">
+            <img src="../../public/Img/LogoBMusic.png" alt="Logo" width="120">
+          </router-link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link :to="{ name: 'instrumental' }" class="header-nav-link">Strumenti</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'sponsor' }" class="header-nav-link">Sponsor</router-link>
+              </li>
+              <li class="nav-item">
+                <a href="http://127.0.0.1:8000/login" class="header-nav-link" @click.prevent="startAnimation($event)">Accedi</a>
+              </li>
+              <li class="nav-item">
+                <a href="http://127.0.0.1:8000/register" class="header-nav-link" @click.prevent="startAnimation($event)">Registrati</a>
+              </li>
+              <li class="nav-item">
+                <router-link :to="{ name: 'search' }" class="header-nav-link">Cerca</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
-</template>
-
+  </template>
 <style lang="scss" scoped>
-
-#container-header {
-    height: 120px;
-    background-color: #21252B;
-}
-
-.container-logo {
-    height: 70px;
-}
-
-.container-logo > img {
-    height: 100%;
-}
-
-nav ul li {
-    list-style: none;
-}
-
-.header-link {
-    font-style: none;
-    text-decoration: none;
-    color: #BADFDA;
-    border-radius: 20px;
-    padding: 10px 20px;
-    margin-left: 5px;
-    &:hover{
-            background-color: white;
-            color: black;
+header {
+background-color: #21252B; color: #BADFDA; border: 1px solid #21252B;
+width: 100%; min-height: 100px;
+display: flex; align-items: center;
+position: sticky; top: 0; left: 0; right: 0; z-index: 999;
+.navbar-nav {
+    display: flex; align-items: center; flex-direction: row; min-height: 100px;
+    .navbar-nav li {
+        list-style: none;
+        &:active{
+            animation: slideOutRight 0.5s ease-out;
         }
+    }
 }
-
-.header-link-special{
-    font-style: none;
-    text-decoration: none;
-    color: black;
-    background-color: #BADFDA;
-    border-radius: 10px;
-    padding: 10px 20px;
-    &:hover{
-            background-color: black;
-            color: #BADFDA;
+.my-navbar {
+    display: flex; align-items: center; justify-content: space-between; flex-grow: 1;
+    .header-nav-link {
+        background-color: #21252B; color: #BADFDA; border-radius: 25px; border: 1px solid #BADFDA;
+        font-weight: bold; text-decoration: none; padding: 15px 25px; margin: 0 15px;
+        position: relative; // Per consentire l'animazione di scorrere all'interno del link
+        transition: transform 0.5s ease-out;
+        &:hover {
+            background-color: #BADFDA; color: #21252B; border-color: 1px solid #21252B;
+            }
         }
-}
+        
+        .navbar-toggler {
+            border: 1px solid #BADFDA; background-color: #21252B; color: #BADFDA;
+            &:focus {
+                outline: none;
+                }
+            .navbar-toggler-icon{
+                background-color: #21252B; color: #BADFDA;
+                &hover{
+                background-color: #BADFDA; border: 1px solid #21252B; color: #21252B;
+                }
+                &:focus {
+                outline: none;
+                }
+            }
 
-// Drop-Down
-.btn-group {
-    display: none;
-}
-
-.dropdown-menu {
-    background-color: #21252B;
-    border: 2px solid #BADFDA;
-}
-
-.link-style {
-    font-style: none;
-    text-decoration: none;
-    margin: 10px 0;
-    color: white;
-    
-}
-
-.dropdown-menu-link{
-    color: #21252B;
-    text-align: center;
-    &:hover{
-            background-color: #BADFDA;
-        }
-}
-
-// Media Query
-
-@media only screen and (max-width: 425px) {
-    #container-header {
-    width: 100%;
-}
-    .header-link, .header-link-special  {
-        display: none;
+        } 
     }
-    .container-logo {
-    height: 50px;
-    }
+}
 
-    .container-logo > img {
-        height: 100%;
-    }
-    .btn-group {
-    display: block;
+
+@keyframes slideOutRight {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(100%);
+  }
 }
-}
+
+
 </style>
